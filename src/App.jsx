777b2902ebfbd824e756db113ac750069a996a13
Bryant Chan <bryantandk@gmail.com>
2023-07-23 11:47:58 +1200

    Init project

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import { styled } from 'styled-components'
+import { urls } from './data'
+import { useState } from 'react'
+
+function App() {
+  const [url, setUrl] = useState(urls[0])
+  const [loading, setLoading] = useState(true)
+
+  const onChangeUrl = (item) => {
+    setLoading(true)
+    setUrl(item)
+  }
+
+  return (
+    <Wrapper>
+      <Sidebar>
+        {urls.map((item) => (
+          <li
+            className={url.url === item.url ? 'selected' : ''}
+            key={item.url}
+            onClick={() => onChangeUrl(item)}
+          >
+            {item.title}
+          </li>
+        ))}
+      </Sidebar>
+      <Iframe>
+        <Loading loading={loading}>loading</Loading>
+        <iframe src={url.url} onLoad={() => setLoading(false)}></iframe>
+      </Iframe>
+    </Wrapper>
+  )
+}
+
+export default App
+
+const Wrapper = styled.div`
+  background: #000;
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+`
+const Sidebar = styled.ul`
+  width: 300px;
+  padding: 20px 0;
+  overflow-y: auto;
+  color: #fff;
+  li {
+    list-style: none;
+    padding: 8px 15px;
+    border-bottom: 1px solid #333;
+    font-size: 14px;
+    cursor: pointer;
+
+    &.selected {
+      background: #333;
+    }
+  }
+`
+const Iframe = styled.div`
+  flex-grow: 1;
+  position: relative;
+  iframe {
+    border: none;
+    width: 100%;
+    height: 100vh;
+  }
+`
+const Loading = styled.div`
+  position: absolute;
+  background: #000;
+  width: 100%;
+  height: 100vh;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: 0.3s all;
+  opacity: ${(props) => (props.loading ? '100%' : '0')};
+`
---
+++ src/data.js
@@ -0,0 +1,342 @@
+export const urls = [
+  {
+    url: 'https://www.coinglass.com/zh/learn/open-interest-zh',
+    title: '什么是未平仓合约（open interest)？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/bitcoin-open-interest-zh',
+    title: '什么是比特币未平仓合约（Bitcoin open interest）？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/oi-1',
+    title: '在加密货币市场中，OI代表什么意思？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/open-interest',
+    title: '未平仓合约（open interest）可以在几个方面影响加密货币市场？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/-1',
+    title: '在加密货币中未平仓合约和交易量有何区别？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/funding-rates-zh',
+    title: '资金费率（Funding rates）',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-3',
+    title: '资金费率和资金费用的区别',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-4',
+    title: '永续合约中的资金费用是怎么计算的？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-5',
+    title: '是什么决定了资金费率？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-6',
+    title: '比特币高资金费率意味着什么？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-7',
+    title: '负资金费率意味着什么？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-8',
+    title: '资金费率套利  （Funding rate arbitrage）',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-9',
+    title: '资金费率的目的是什么？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-10',
+    title: '如何避免在加密货币交易中爆仓？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-11',
+    title: '清算价格和破产价格的区别',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-12',
+    title: '当你在加密期货交易中被清算时钱去哪里了?',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-13',
+    title: '加密货币中现货交易和合约交易有什么差异？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-15',
+    title: '什么是风险保障基金？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-16',
+    title: '如何管理加密合约交易中的风险 ?',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-18',
+    title: '什么是U本位合约和币本位合约以及它们的区别？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-19',
+    title: '永续合约与传统合约的差异?',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-21',
+    title: '什么是自动减仓？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-22',
+    title: '交割合约的清算与交割',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-23',
+    title: '  订单薄',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-24',
+    title: '什么是市场深度？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-25',
+    title: '什么是加密合约中的标记价？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-26',
+    title: '爆仓、清算（liquidation、REKT）',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-27',
+    title: '加密货币期货计算器',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-28',
+    title: '跟踪委托',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-29',
+    title: '什么是市价单和限价单价格上限和下限比率？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-30',
+    title: '什么是止盈止损单？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-31',
+    title: '什么是基差（Basis）？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-33',
+    title: '限价定单（Limit Order）',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-34',
+    title: '永续合约指标基础',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-35',
+    title: '美元指数（DYX）与比特币价格波动的关系',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-36',
+    title: '加密货币交易和分析的十佳指标',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-37',
+    title: '最佳加密货币交易策略',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-38',
+    title: '投资加密货币和交易加密货币那种方式更适合你？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-39',
+    title: '如何使用量价分析（Volume Profile）交易比特币？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-41',
+    title: '什么是Stochastic Oscillator（随机振荡指标）？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-42',
+    title: '什么是Cumulative Volume Delta（CVD）？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-43',
+    title: '什么是轧空(SHORT SQUEEZE)？轧空出现的原因？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-44',
+    title: 'Volume Delta和Cumulative Volume Delta（CVD）有什么区别？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-45',
+    title: '加密期货交易的常见错误与如何避免',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-47',
+    title: '加密期货交易中的订单流量和市场深度',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-49',
+    title: '如何在加密货币交易中选择最适合您的交易风格？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-48',
+    title: '加密期货交易中的交易心理学：如何管理情绪和避免过度交易？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-51',
+    title: '如何准备并应对加密期货交易中的黑天鹅事件？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/-4',
+    title: '加密期货交易中如何用布林带测量价格波动性？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-52',
+    title: '加密期货交易中的相对强弱指数（RSI）分析：如何确定市场趋势？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-53',
+    title: '永续合约交易和杠杆交易：有什么不同？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-54',
+    title: '比特币Ahr999指标',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/-6',
+    title: '如何计算最大痛点价格？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-56',
+    title: '期权的最大痛点价格',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-58',
+    title: '十种常见的期权交易策略',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-60',
+    title: '期权的主要类型有哪些？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-62',
+    title: '交易期权的提示',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-64',
+    title: '什么是长期期权（ Leap Options）？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-66',
+    title: '怎么使用期权来对冲？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-67',
+    title: '什么是虚值期权Out-of-the-Money（OTM）？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-68',
+    title: '平值期权 At-the-money (ATM)',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-70',
+    title: '实值期权In-the-Money（ITM）',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-71',
+    title: 'Options Volume（期权交易量）',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-72',
+    title: '哪些因素决定了期权定价？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-73',
+    title: '什么是期权定价？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-74',
+    title: '基础价格与行权价格',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-76',
+    title: '利率（ Interest Rates）',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-78',
+    title: '什么是波动率（Volatility）？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-80',
+    title: '时间到期（time to Expiration）',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-81',
+    title: 'Bid和Ask',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-82',
+    title: '到期日（Expiration Date）',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-83',
+    title: 'Vega',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-84',
+    title: 'Theta',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-85',
+    title: ' Delta',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-86',
+    title: '加密期货交易中的资金费率如何在交易中运用？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-87',
+    title: '加密期货交易中的趋势线分析：如何利用趋势线制定交易计划？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-88',
+    title: '加密期货交易中的基本分析：什么是关键因素？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-89',
+    title: '如何使用技术分析在加密期货交易中做出明智的决策？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-90',
+    title: '加密期货交易中的技术分析和基本分析：哪种更适合？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-91',
+    title: '加密期货交易中如何使用算法交易？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-92',
+    title: '加密永续合约交易中有哪些交易成本？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-93',
+    title: '什么是交易深度（depth of market）？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-95',
+    title: '什么是ATH？历史最高价（All Time High，缩写为ATH）',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-96',
+    title: '什么是比特币合约多空比？',
+  },
+  {
+    url: 'https://www.coinglass.com/zh/learn/learn-116',
+    title: '什么是加密货币爆仓（liquidation）？',
+  },
+]
